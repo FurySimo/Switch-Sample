121a68e609e24c3471db3d15843734a6001f5423
Z M Asif Jamil <80680790+FurySimo@users.noreply.github.com>
2021-03-17 20:17:27 +0600

    Add files via upload

---
+++ Switch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your day:");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Wednesday");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Thursday");
+                    break;
+                default:
+                    Console.WriteLine("End of Week");
+                    break;
+
+
+
+            }
+        }
+    }
+}
+
